eae083be6358ae8bed59b2ff33675fa6e0de08d5
nicholaas2cheung <nicholascheung25.gtd@gmail.com>
2021-02-15 00:31:32 +0800

    finish refactoring ColorBox Css

--- src/ColorBox.jsx
+++ src/ColorBox.jsx
@@ -85,6 +85,42 @@ const styles = {
         transform: 'scale(50)',
         zIndex: '10',
         position: 'absolute'
+    },
+    copyMessage: {
+        position: 'fixed',
+        top: '0',
+        left: '0',
+        bottom: '0',
+        right: '0',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '4rem',
+        transform: 'scale(0.1)',
+        opacity: '0',
+        color: 'white',
+        '& h1': {
+            fontWeight: '400',
+            textShadow: '1px 2px black',
+            background: 'rgba(255, 255, 255, 0.15)',
+            width: '100%',
+            textAlign: 'center',
+            marginBottom: '0px',
+            padding: '1rem',
+            textTransform: 'uppercase'
+        },
+        '& p': {
+            fontSize: '2rem',
+            fontWeight: '100'
+        }
+    },
+    showMessage: {
+        opacity: '1',
+        transform: 'scale(1)',
+        zIndex: '25',
+        transition: 'all 0.4s ease-in-out',
+        transitionDelay: '0.1s'
     }
 };
 
@@ -113,7 +149,10 @@ class ColorBox extends Component {
                             this.state.copied && classes.showOverlay
                         }`}
                     />
-                    <div className={`copy-msg ${this.state.copied && 'show'}`}>
+                    <div
+                        className={`${classes.copyMessage} ${
+                            this.state.copied && classes.showMessage
+                        }`}>
                         <h1>copied!</h1>
                         <p className={classes.copyText}>{background}</p>
                     </div>
