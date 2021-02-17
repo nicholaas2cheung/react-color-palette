0b9248de1591733ff6bb8a98074a0fba182c1a61
nicholaas2cheung <nicholascheung25.gtd@gmail.com>
2021-02-17 15:03:44 +0800

    styled color picker form

--- src/ColorPickerForm.jsx
+++ src/ColorPickerForm.jsx
@@ -3,6 +3,27 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import { ChromePicker } from 'react-color';
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+    root: {
+        width: '100%'
+    },
+    picker: {
+        width: '100% !important',
+        marginTop: '2rem'
+    },
+    addColor: {
+        width: '100%',
+        padding: '1rem',
+        marginTop: '1rem',
+        fontSize: '2rem'
+    },
+    colorNameInput: {
+        width: '100%',
+        height: '70px'
+    }
+};
 
 class ColorPickerForm extends Component {
     constructor(props) {
@@ -45,14 +66,22 @@ class ColorPickerForm extends Component {
     };
 
     render() {
-        const { paletteIsFull } = this.props;
+        const { paletteIsFull, classes } = this.props;
         const { currentColor, newColorName } = this.state;
         return (
-            <div>
-                <ChromePicker color={currentColor} onChangeComplete={this.updateCurrentColor} />
+            <div className={classes.root}>
+                <ChromePicker
+                    color={currentColor}
+                    onChangeComplete={this.updateCurrentColor}
+                    className={classes.picker}
+                />
                 <ValidatorForm onSubmit={this.handleSubmit} ref="form">
                     <TextValidator
                         value={newColorName}
+                        placeholder="Color Name"
+                        margin="normal"
+                        variant="filled"
+                        className={classes.colorNameInput}
                         name="newColorName"
                         onChange={this.handleChange}
                         validators={['required', 'isColorNameUnique', 'isColorUnique']}
@@ -66,6 +95,7 @@ class ColorPickerForm extends Component {
                         variant="contained"
                         color="secondary"
                         type="submit"
+                        className={classes.addColor}
                         disabled={paletteIsFull}
                         style={{
                             backgroundColor: paletteIsFull ? 'grey' : currentColor
@@ -78,4 +108,4 @@ class ColorPickerForm extends Component {
     }
 }
 
-export default ColorPickerForm;
+export default withStyles(styles)(ColorPickerForm);
--- src/NewPaletteForm.jsx
+++ src/NewPaletteForm.jsx
@@ -19,23 +19,6 @@ const styles = (theme) => ({
     root: {
         display: 'flex'
     },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        })
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen
-        })
-    },
-    menuButton: {
-        marginRight: theme.spacing(2)
-    },
     hide: {
         display: 'none'
     },
@@ -44,7 +27,9 @@ const styles = (theme) => ({
         flexShrink: 0
     },
     drawerPaper: {
-        width: drawerWidth
+        width: drawerWidth,
+        display: 'flex',
+        alignItems: 'center'
     },
     drawerHeader: {
         display: 'flex',
@@ -70,6 +55,20 @@ const styles = (theme) => ({
             duration: theme.transitions.duration.enteringScreen
         }),
         marginLeft: 0
+    },
+    container: {
+        width: '90%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttons: {
+        width: '100%'
+    },
+    button: {
+        width: '50%'
     }
 });
 
@@ -147,7 +146,6 @@ class NewPaletteForm extends Component {
             <div className={classes.root}>
                 <PaletteFormNav
                     open={open}
-                    classes={classes}
                     palettes={palettes}
                     handleSubmit={this.handleSubmit}
                     handleDrawerOpen={this.handleDrawerOpen}
@@ -166,24 +164,33 @@ class NewPaletteForm extends Component {
                         </IconButton>
                     </div>
                     <Divider />
-                    <Typography variant="h4">Design Your Palette</Typography>
-                    <div>
-                        <Button variant="contained" color="secondary" onClick={this.clearColors}>
-                            Clear Palette
-                        </Button>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={this.addRandomColor}
-                            disabled={paletteIsFull}>
-                            Random Color
-                        </Button>
+                    <div className={classes.container}>
+                        <Typography variant="h4" gutterBottom>
+                            Design Your Palette
+                        </Typography>
+                        <div className={classes.buttons}>
+                            <Button
+                                variant="contained"
+                                color="secondary"
+                                className={classes.button}
+                                onClick={this.clearColors}>
+                                Clear Palette
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className={classes.button}
+                                onClick={this.addRandomColor}
+                                disabled={paletteIsFull}>
+                                Random Color
+                            </Button>
+                        </div>
+                        <ColorPickerForm
+                            paletteIsFull={paletteIsFull}
+                            addNewColor={this.addNewColor}
+                            colors={colors}
+                        />
                     </div>
-                    <ColorPickerForm
-                        paletteIsFull={paletteIsFull}
-                        addNewColor={this.addNewColor}
-                        colors={colors}
-                    />
                 </Drawer>
                 <main
                     className={clsx(classes.content, {
